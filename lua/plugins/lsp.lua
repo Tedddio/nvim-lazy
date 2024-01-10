47370002330e74486f766523b88eaac883f6620c
Cedrick Therrien <therrien.cedrick@gmail.com>
2024-01-09 20:02:39 -0500

    feat: added fidget

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,22 @@ return {
                 end,
             })
 
+            lspconfig.html.setup({
+                capabilities = capabilities,
+                filetypes = { "html", "templ" },
+            })
+
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+                init_options = { userLanguages = { templ = "html" } },
+            })
+
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+                filetypes = { "go", "templ" },
+            })
+
             vim.keymap.set("n", "gd", function()
                 vim.lsp.buf.definition()
             end, {})
